e1d47786c817a073e9530e39878184daa06d5fb1
Florian Bollmann <florian@gentlent.com>
2020-03-20 19:22:05 +0100

    create homepage

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function checkbox(){
+  if (document.getElementById("checkbox").checked == true) {
+  document.getElementById("meme").style.display = "inline";
+  } else {
+    document.getElementById("meme").style.display = "none";
+  }
+}
